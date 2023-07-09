435546a5617e3924130ce82839caa48ce6898595
Brucehml <61675330+Brucehml@users.noreply.github.com>
2023-07-09 15:29:36 +0800

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,21 @@
+import time
+import tkinter as tk
+ 
+def tick():
+    # 获取当前时间
+    current_time = time.strftime('%H:%M:%S')
+    # 更新标签文本
+    clock_label.config(text=current_time)
+    # 每1秒调用一次tick函数
+    clock_label.after(1000, 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
