fb932fad74de95d5933c753049a6abbd9d8d7cc8
Brucehml <61675330+Brucehml@users.noreply.github.com>
2023-08-10 11:36:36 +0800

    Create gu.py

---
+++ gu.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import seaborn.linearmodels as snsl
+from datetime import datetime
+import tushare as ts
+sns.set_style("whitegrid")
+end = datetime.today() #开始时间结束时间，选取最近一年的数据
+start = datetime(end.year-1,end.month,end.day)
+end = str(end)[0:10]
+start = str(start)[0:10]
+stock = ts.get_hist_data('300104',start,end)#选取一支股票
+stock['close'].plot(legend=True ,figsize=(10,4))
+plt.show()
