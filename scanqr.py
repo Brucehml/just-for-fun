6f861917a3c9b93d49b3c681e8edadd4b8975991
Brucehml <61675330+Brucehml@users.noreply.github.com>
2024-04-12 21:49:33 +0800

    Update scanqr.py

--- scanqr.py
+++ scanqr.py
@@ -2,8 +2,6 @@ import numpy as np
 from PIL import Image
 from pyzbar import pyzbar
 import cv2
-
-
 # 读取二维码信息
 def OR_read(img):
     # 读取文件，转成数组
@@ -14,8 +12,6 @@ def OR_read(img):
     else:
         return None
     # return pyzbar.decode(im)[0].data.decode("utf-8")
-
-
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 while cap.isOpened():
     ok, frame = cap.read()  # 读取一帧数据
